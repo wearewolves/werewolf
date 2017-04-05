1e5a5f4dc35b73bd7572e6bd95ceb29be2b4a4d0
kijuli <rekenber@gmail.com>
2017-04-06 05:03:56 +0900

    테스트 및 배포 설정 테스트

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-werewolf_30m
+[![Build Status](https://travis-ci.org/wearewolves/werewolf.svg?branch=master)](https://travis-ci.org/wearewolves/werewolf)
+
+# werewolf6.cafe24.co.kr
+"인랑"의 "30분 서버"의 소스입니다.
+
+## 개발
+소스 공개를 통해서 인랑 유저분들과 함께 인랑을 개발 할 수 있는 환경을 만들었습니다.
+
+저장소를 fork해서, 소스를 수정한 후 pull requsts를 보내주시면 30분 마을에 반영이 됩니다.
+
+pull requsts를 보내주시는 분은 모두 커미터로 반영할 생각입니다.
+
+## 서버 구축
+* 공개된 소스를 가지고 별도의 서버를 만드실 수도 있습니다.
+* 서버 구축 메뉴얼은 작성하지 못했습니다.
---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var ftp = require('vinyl-ftp');
+var gutil = require('gulp-util');
+var minimist = require('minimist');
+var args = minimist(process.argv.slice(2));
+gulp.task('deploy', function() {
+  var remotePath = '/www/';
+  console.log("deploy " + args.branch+" "+args.tag)
+  var conn = ftp.create({
+    host: 'werewolf6.cafe24.com',
+    user: args.user,
+    password: args.password,
+    log: gutil.log
+  });
+  gulp.src(['README.md'])
+    .pipe(conn.newer(remotePath))
+    .pipe(conn.dest(remotePath));
+});
+

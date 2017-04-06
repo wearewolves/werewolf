6980b209a39dfe56330631a3c786c0ead71c0f02
kijuli <rekenber@gmail.com>
2017-04-07 00:15:24 +0900

    chmod 설정 추가

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var ftp = require('vinyl-ftp');
 var gutil = require('gulp-util');
+const chmod = require('gulp-chmod');
 var minimist = require('minimist');
 var args = minimist(process.argv.slice(2));
 gulp.task('deploy', function() {
@@ -19,6 +20,10 @@ gulp.task('deploy', function() {
     .pipe(conn.newer(remotePath + 'Werewolf/'))
     .pipe(conn.dest(remotePath + 'Werewolf/'));
   gulp.src(['server/**/*.*'])
+    .pipe(conn.newer(remotePath + 'server'))
+    .pipe(conn.dest(remotePath + 'server'));
+  gulp.src(['server/next.cgi'])
+    .pipe(chmod(0o755))
     .pipe(conn.newer(remotePath + 'server'))
     .pipe(conn.dest(remotePath + 'server'));
 });

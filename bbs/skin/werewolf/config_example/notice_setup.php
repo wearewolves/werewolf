518b90097f050e48add680377e43b9b83434d7f3
Arepin <Arepin@users.noreply.github.com>
2017-04-09 23:17:28 +0900

    notice 수정
    링크 수정

--- bbs/skin/werewolf/config_example/notice_setup.php
+++ bbs/skin/werewolf/config_example/notice_setup.php
@@ -1,7 +1,7 @@
 <?
 	//전체 공지
 	$notice[1]="<b>[신규 룰 '익스펜션'이 추가되었습니다!!!]</b>
-	자세한 내용은 <a href='http://werewolf.co.kr/bbs/view.php?id=rule&page=1&sn1=&divpage=1&sn=off&ss=on&sc=on&select_arrange=headnum&desc=asc&no=12'>[인원 구성]</a>,<a href='http://werewolf.co.kr/bbs/view.php?id=rule&page=1&sn1=&divpage=1&sn=off&ss=on&sc=on&select_arrange=headnum&desc=asc&no=13'>[직업별 능력]</a>,<a href='http://werewolf.co.kr/bbs/view.php?id=rule&page=1&sn1=&divpage=1&sn=off&ss=on&sc=on&select_arrange=headnum&desc=asc&no=15'>[플레이 가이드]</a>,<a href='http://werewolf.co.kr/bbs/view.php?id=rule&page=1&sn1=&divpage=1&sn=off&ss=on&sc=on&select_arrange=headnum&desc=asc&no=22'>[시스템 메시지]</a>를 참조해 주세요.	
+	자세한 내용은 <a href='http://werewolf.co.kr/bbs/view.php?id=rule&no=12'>[인원 구성]</a>,<a href='http://werewolf.co.kr/bbs/view.php?id=rule&no=13'>[직업별 능력]</a>,<a href='http://werewolf.co.kr/bbs/view.php?id=rule&no=15'>[플레이 가이드]</a>,<a href='http://werewolf.co.kr/bbs/view.php?id=rule&no=22'>[시스템 메시지]</a>를 참조해 주세요.	
 건의 사항은 <a href='http://werewolf.co.kr/bbs/zboard.php?id=rule'>[룰 토론 게시판]</a>을 이용해 주세요. [2008-11-01 15:20] 
 
 	<b>[ 지정되지 않은 오류 발생시 해결 방법 ]</b>
@@ -11,8 +11,8 @@
 	$notice[1] = nl2br($notice[1]);
 
 	// 초보자 공지
-	$noviceNotice[1] = "게임에 참여하기 전에 <a href='http://werewolf.springnote.com/pages/464186' target='blank'>[매뉴얼]</a>과 <strong><a href='skin/werewolf/view_best_village.php?id=werewolf' target='blank'>[종료된 마을]</a></strong>을 보고 게임 진행과 분위기를 익혀 주세요.";
-	$noviceNotice[2] = "이곳은 24시간 마을만 플레이할 수 있는 서버입니다.<br>마을에 참여하기 위해서는 <a href='http://werewolf.springnote.com/pages/464186#newplayer'>[신규 회원 가입에 대한]</a> 글을 참고해 주세요.";
+	$noviceNotice[1] = "게임에 참여하기 전에 <a href='http://ko.werewolf.wikia.com' target='blank'>[매뉴얼]</a>과 <strong><a href='skin/werewolf/view_best_village.php?id=werewolf' target='blank'>[종료된 마을]</a></strong>을 보고 게임 진행과 분위기를 익혀 주세요.";
+	$noviceNotice[2] = "이곳은 24시간 마을만 플레이할 수 있는 서버입니다.<br>마을에 참여하기 위해서는 <a href='http://ko.werewolf.wikia.com/wiki/%EC%9B%B9%EA%B2%8C%EC%9E%84_%EC%9D%B8%EB%9E%91%EC%97%90_%EC%96%B4%EC%84%9C%EC%98%A4%EC%84%B8%EC%9A%94'>[신규 회원 가입에 대한]</a> 글을 참고해 주세요.";
 	$noviceNotice[3] = "인랑에 처음 오셨다면 2, 3서버에서 게임을 즐겨 보시기 바랍니다.<br> 2, 3 서버에서는 바로 게임에 참여할 수 있습니다.";
 	$noviceNotice[5] = "<span class='alert'>게임을 '처음'한다는 발언은 금지입니다. <br />처음 게임을 하게 되면 마을 사람이 됩니다.그렇기 때문에 처음 게임을 한다고 말하게 되면 마을 사람인걸 알게 되기 때문에 말하시면  안됩니다.</span>";
 

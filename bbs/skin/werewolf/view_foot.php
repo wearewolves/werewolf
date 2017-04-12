f0783a687de431afc3ca1bec7a034d2ee9db8c71
Jakaline <dol2848@naver.com>
2017-04-12 21:48:03 +0900

    hotfix#6

--- bbs/skin/werewolf/view_foot.php
+++ bbs/skin/werewolf/view_foot.php
@@ -343,8 +343,9 @@ function formcheck(f){
 			<div id="rolebox">
 				<select name='selectCharacter' class="role_select">
 				<?
+				$stlink = 'http://werewolf6.cafe24.com/bbs/';
 				for($rolecount=0;$mirei = mysql_fetch_array($characterQuery);$rolecount++){
-					echo "<option data-img-src='werewolf6.cafe24.com/bbs/".$characterImageFolder.$mirei['half_image']."' value='".$mirei['no']."' >".$character_list[$mirei['no']]."</option>";
+					echo "<option data-img-src='".$stlink.$characterImageFolder.$mirei['half_image']."' value='".$mirei['no']."' >".$character_list[$mirei['no']]."</option>";
 				}
 				?>
 				</select>

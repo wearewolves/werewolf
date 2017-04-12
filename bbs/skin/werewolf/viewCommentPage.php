bc972344b86dabd6f12bc93c26f19f90cb046a62
Arepin <arepin23@gmail.com>
2017-04-13 02:50:40 +0900

    Fix seal logs printing bug +
    Fix seal logs printing bug +

--- bbs/skin/werewolf/viewCommentPage.php
+++ bbs/skin/werewolf/viewCommentPage.php
@@ -209,8 +209,8 @@ if(substr_count ( $UNSID,"<||>") == 4){
 			if($viewChar and is_numeric($viewChar)) $checkChar = " AND `character` = $viewChar ";
 			// Hide seal logs until the end of game except for myself and admin
 			if($checkChar)
-				// viewChar != playing character && not admin
-				if($viewChar != $character && !$is_admin) $checkChar .= "AND type != '봉인제안' ";
+				// game in progress && viewChar != playing character && not admin
+				if($gameinfo['state'] == "게임중" && $viewChar != $character && !$is_admin) $checkChar .= "AND type != '봉인제안' ";
 			if(!$member[no]) $member[no] = 0;
 			
 			if($entry and $gameinfo['state'] == "게임중"){

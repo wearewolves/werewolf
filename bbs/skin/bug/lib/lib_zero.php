58538db3e1c20510b7419753f82b075ee0422c08
Arepin <arepin23@gmail.com>
2017-04-12 18:43:29 +0900

    Fix autolink bug
    Fix autolink bug

--- bbs/skin/bug/lib/lib_zero.php
+++ bbs/skin/bug/lib/lib_zero.php
@@ -883,7 +883,7 @@ $REQUEST_URI = "$PHP_SELF"."$_QU"."$QUERY_STRING";
 	// URL, Mail을 자동으로 체크하여 링크만듬
 	function autolink($str) {
 		// URL 치환
-		$homepage_pattern = "/([^\"\'\=\>])(mms|http|HTTP|ftp|FTP|telnet|TELNET)\:\/\/(.[^ \n\<\"\']+)/";
+		$homepage_pattern = "/([^\"\'\=\>])(mms|http|HTTP|https|HTTPS|ftp|FTP|telnet|TELNET)\:\/\/(.[^ \n\<\"\']+)/";
 		$str = preg_replace($homepage_pattern,"\\1<a href=\\2://\\3 target=_blank>\\2://\\3</a>", " ".$str);
 
 		// 메일 치환

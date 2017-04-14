15307490f5b8fd754d71c32c073b8d7a30915570
ChungkeunLee <elkein@snu.ac.kr>
2017-04-14 10:52:57 +0900

    expansionrule errita fixed (werewolfRace to werewolfPlayers)

--- server/werewolf/game/rule/ExpensionRule.py
+++ server/werewolf/game/rule/ExpensionRule.py
@@ -162,16 +162,15 @@ class ExpansionRule(WerewolfRule):
             lonelywerewolfPlayer = lonelywerewolfPlayer[0]
         
         #살아 있는 인랑이 있을 때만 습격을 진행한다.
-        if not werewolfRace and (not readerwerewolfPlayer or readerwerewolfPlayer.alive=="사망") \
+        if not werewolfPlayers and (not readerwerewolfPlayer or readerwerewolfPlayer.alive=="사망") \
                             and (not lonelywerewolfPlayer or lonelywerewolfPlayer.alive =="사망"):
             return        
 
         #인랑들이 습격을 결정했는지 확인한다.
-        if not werewolfRace:
-            for werewolfPlayer in werewolfPlayers:
-                #습격을 안했다면! 랜덤 습격 시작     
-                if not werewolfPlayer.hasAssault():
-                    werewolfPlayer.assaultRandom(humanRace)
+        for werewolfPlayer in werewolfPlayers:
+            #습격을 안했다면! 랜덤 습격 시작     
+            if not werewolfPlayer.hasAssault():
+                werewolfPlayer.assaultRandom(humanRace)
         if readerwerewolfPlayer and readerwerewolfPlayer.alive=="생존":
             if not readerwerewolfPlayer.hasAssault():
                 readerwerewolfPlayer.assaultRandom(humanRace)

b7192940f7961d7ecffd1028a246c5d953d44764
Arepin <arepin23@gmail.com>
2017-04-20 00:02:12 +0900

    Fix layouts
    Fix layouts

--- bbs/skin/werewolf/write.php
+++ bbs/skin/werewolf/write.php
@@ -427,8 +427,8 @@ function changeTermOfDay(obj){
 		<table border=0 cellspacing=1 cellpadding=2 width=100% height=40>
 		<tr>
 			<td align="right">
-				<input type="button" value="" src="skin/werewolf/image/cancel.gif" border="0" onfocus="blur()" style="width:121px; height:61px; background-image:url(skin/werewolf/image/cancel.gif); float:right; cursor:pointer;" onclick="javascript:void(history.back())">
-				&nbsp;&nbsp;<input type="submit" value="" src="skin/werewolf/image/ok.gif" border="0" onfocus="blur()" style="width:58px; height:61px; background-image:url(skin/werewolf/image/ok.gif); float:right; cursor:pointer;" accesskey="s">
+				<input type="submit" value="" src="skin/werewolf/image/ok.gif" border="0" onfocus="blur()" style="width:58px; height:61px; background-image:url(skin/werewolf/image/ok.gif); display:inline-block; cursor:pointer;" accesskey="s">
+				&nbsp;&nbsp;<input type="button" value="" src="skin/werewolf/image/cancel.gif" border="0" onfocus="blur()" style="width:121px; height:61px; background-image:url(skin/werewolf/image/cancel.gif); display:inline-block; cursor:pointer;" onclick="javascript:void(history.back())">
 			</td>
 		</tr>
 		</table>

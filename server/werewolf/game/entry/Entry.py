b8cc840021389ece110b629be35dac1af7414963
ChungkeunLee <elkein@snu.ac.kr>
2017-05-02 15:11:42 +0900

    Revert "no werewolf, add telepathy"

--- server/werewolf/game/entry/Entry.py
+++ server/werewolf/game/entry/Entry.py
@@ -127,6 +127,10 @@ class Entry:
         query %= (self.game.game)
         logging.debug(query)
         cursor.execute(query)
+        query2 = "update `zetyx_board_werewolf_entry` set telepathy = '0' where game = '%s'"
+        query2 %= (self.game.game)
+        logging.debug(query2)
+        cursor.execute(query2)
 
     def makePlayer(self, entryList):
         resultList = []

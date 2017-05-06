ac757fb16e5d6d93b5595fb80a86e6452cdd430c
kijuli <rekenber@gmail.com>
2017-05-06 14:12:46 +0900

    배포 오류 수정

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-COMMIT=`git rev-list --tags --skip=1  --max-count=1`
+COMMIT=`git log -1 --no-walk --tags --pretty="%h" --skip=1`
 BEFORE_TAG=`git describe --abbrev=0 --tags $COMMIT`
 `git diff $BEFORE_TAG $3 --name-only |  xargs tar -zcf release.tar.gz`
 `mkdir release`

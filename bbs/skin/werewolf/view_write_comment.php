a023088c7a67851b01f7e105d1106fc5dfdb8dbf
Jakaline <dol2848@naver.com>
2017-05-13 22:17:42 +0900

    Add files via upload

--- bbs/skin/werewolf/view_write_comment.php
+++ bbs/skin/werewolf/view_write_comment.php
@@ -121,7 +121,7 @@ if($totalCommentPage>=1) echo "</div>";
 		 <?=$hide_c_password_end?>
 	  <tr bgcolor=111111>
 			<td width=100%>
-			<div name="memoedit" id="memoedit" style="min-height:50px;background-color:white;color:#000;display:inline-block;" contenteditable></div>
+			<div name="memoedit" id="memoedit" style="min-height:50px;max-width:100%;background-color:white;color:#000;display:inline-block;" contenteditable></div>
 			<input type="hidden" name="memo" id="memo">
 			</td>
 			

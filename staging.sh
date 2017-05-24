d8a9a844b0bb9a1c83bac585969351929ebf69b3
kijuli <rekenber@gmail.com>
2017-05-25 02:25:49 +0900

    압축 결과 출력하기

--- staging.sh
+++ staging.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-`git diff HEAD~1 HEAD --name-only |  xargs tar -zcf release.tar.gz`
-`mkdir release`
-`tar -xvzf release.tar.gz -C release`
-ls
+git diff HEAD~1 HEAD --name-only |  xargs tar -ztvcf release.tar.gz
+mkdir release
+tar -xtvzf release.tar.gz -C release
+
 gulp staging --user $1 --password $2

9ef6a0573405490c54525027ac8660bcbb6e1848
kijuli <rekenber@gmail.com>
2017-05-25 02:53:45 +0900

    압축..

--- staging.sh
+++ staging.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git diff HEAD~1 HEAD --name-only |  xargs tar -zvcf release.tar.gz
+git diff HEAD~1 HEAD --name-only |  xargs tar -zcf release.tar.gz
 mkdir release
 tar -xvzf release.tar.gz -C release
-
+ls release
 gulp staging --user $1 --password $2

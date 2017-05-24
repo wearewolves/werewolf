33f6459435ee3feeaa6ee50e8cacc1d336ecc751
kijuli <rekenber@gmail.com>
2017-05-25 02:31:13 +0900

    압축 내용 출력

--- staging.sh
+++ staging.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git diff HEAD~1 HEAD --name-only |  xargs tar -ztvcf release.tar.gz
+git diff HEAD~1 HEAD --name-only |  xargs tar -zvcf release.tar.gz
 mkdir release
-tar -xtvzf release.tar.gz -C release
+tar -xvzf release.tar.gz -C release
 
 gulp staging --user $1 --password $2

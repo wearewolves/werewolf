4c268f9fcfc7dc924ecfffa7a47b5d52ceaf6626
Arepin <arepin23@gmail.com>
2017-06-05 01:48:59 +0900

    Roll-back
    Roll-back

--- bbs/skin/werewolf/js/werewolf-role-playing-set.js
+++ bbs/skin/werewolf/js/werewolf-role-playing-set.js
@@ -102,7 +102,6 @@ function searchRPSet() {
     // Loop through all list items, and hide those who don't match the search query
     for (i = 0; i < li.length; i++) {
         if (li[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.fontSize = "16px";
             li[i].style.display = "";
         } else {
             li[i].style.display = "none";

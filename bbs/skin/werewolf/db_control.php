69c04e7a6905308fa506665e4281838d435c3e50
epi <episoty@gmail.com>
2017-06-21 22:25:32 +0900

    insert 제거

--- bbs/skin/werewolf/db_control.php
+++ bbs/skin/werewolf/db_control.php
@@ -1,18 +1,17 @@
 <?
-	// register_globals°¡ offÀÏ ¶§¸¦ À§ÇØ º¯¼ö ÀçÁ¤ÀÇ
+	// register_globalsê°€ offì¼ ë•Œë¥¼ ìœ„í•´ ë³€ìˆ˜ ìž¬ì •ì˜
 	@extract($HTTP_GET_VARS); 
 	@extract($HTTP_POST_VARS); 
 	@extract($HTTP_SERVER_VARS);
 	@extract($HTTP_ENV_VARS);
 
-	// Á¦·Îº¸µå ¶óÀÌºê·¯¸® °¡Á®¿È
+	// ì œë¡œë³´ë“œ ë¼ì´ë¸ŒëŸ¬ë¦¬ ê°€ì ¸ì˜´
 	$_zb_path = realpath("../../")."/";
 	include $_zb_path."lib.php";
 
-	// DB ¿¬°áÁ¤º¸ °¡Á®¿È
+	// DB ì—°ê²°ì •ë³´ ê°€ì ¸ì˜´
 	$connect = dbConn();
 
-	mysql_query("INSERT INTO `zetyx_board_werewolf_rule` (`no`, `name`, `min_player`, `max_player`) VALUES (4, '½Å·Úµµ', 11, 17);");
 	mysql_query("update `zetyx_board_werewolf_rule` set min_player = '8', max_player = '9' where no = '5';");
 	
 	mysql_close($connect);

adebd045193af91a11fb9e1ff9a5d79a449d6091
epi <epi4@github.com>
2017-06-22 10:08:50 +0900

    revert

--- server/werewolf/game/rule/ExpensionRule.py
+++ server/werewolf/game/rule/ExpensionRule.py
@@ -7,12 +7,12 @@ from werewolf.game.entry.Role import Truecharacter
 from werewolf.game.entry.Role import Race
 from werewolf.game.rule.Rule import WerewolfRule
 
-class ExpensionRule(WerewolfRule):
+class ExpansionRule(WerewolfRule):
     def __init__(self, game):
-        super(ExpensionRule, self).__init__(game)
+        super(ExpansionRule, self).__init__(game)
         self.min_players = 9
         self.max_players = 17
-        logging.debug("expension rule")
+        logging.debug("expansion rule")
 
     def getTruecharacterList(self, number):
         if number < 11:
@@ -51,7 +51,7 @@ class ExpensionRule(WerewolfRule):
         return rolelist
 
     def initGame(self):
-        logging.info("init Expension Rule")
+        logging.info("init Expansion Rule")
         WerewolfRule.initGame(self)
     
     def writePlayerWill(self):

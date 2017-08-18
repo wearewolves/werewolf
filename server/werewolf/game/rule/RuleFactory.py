350154f8b6b0d8ea1e878ef272352b1b93f6a997
epi <mjlee@innosim.com>
2017-08-18 16:20:01 +0900

    revert test

--- server/werewolf/game/rule/RuleFactory.py
+++ server/werewolf/game/rule/RuleFactory.py
@@ -7,7 +7,6 @@ class RULE_NAME:
     HAMSTER = 2
     EXPANSION = 3
     CONFIDENCE = 4
-    INSTANT = 5
 
 class SUBRULE_NAME:
     ASSAULT_ONESELF = 1
@@ -40,7 +39,4 @@ class RuleFactory:
         elif rule == RULE_NAME.CONFIDENCE:
             from werewolf.game.rule.ConfidenceRule import ConfidenceRule
             return ConfidenceRule(game)
-        elif rule == RULE_NAME.INSTANT:
-            from werewolf.game.rule.InstantRule import InstantRule
-            return InstantRule(game)
     getRule = staticmethod(getRule)

7ed10ba82861c7977ae347ed091b2636fc3ba3f8
epi <epi4@github.com>
2017-08-23 02:02:37 +0900

    1server 적용

--- gulpfile.js
+++ gulpfile.js
@@ -3,38 +3,22 @@ var ftp = require('vinyl-ftp');
 var gutil = require('gulp-util');
 var minimist = require('minimist');
 var args = minimist(process.argv.slice(2));
-
-function release(conn) {
+gulp.task('deploy', function() {
   var remotePath = '/www/';
+  var conn = ftp.create({
+    host: 'werewolf.co.kr',
+    user: args.user,
+    password: args.password,
+    log: gutil.log,
+    maxConnections: 4
+  });
 
   gulp.src(['release/**/*.*'])
     .pipe(conn.newer(remotePath))
-    .pipe(conn.dest(remotePath));  
+    .pipe(conn.dest(remotePath));
+
   gulp.src(['server/next.cgi'])
     .pipe(conn.mode(remotePath +'server','0755'))
     .pipe(conn.newer(remotePath + 'server'))
     .pipe(conn.dest(remotePath + 'server'));
-}
-
-gulp.task('deploy', function() {
-  var conn = ftp.create({
-    host: 'werewolf6.cafe24.com',
-    user: args.user,
-    password: args.password,
-    log: gutil.log,
-    maxConnections: 4
-  });
-
-  release(conn)
-});
-gulp.task('staging', function() {
-  var conn = ftp.create({
-    host: 'werewolf7.cafe24.com',
-    user: args.user,
-    password: args.password,
-    log: gutil.log,
-    maxConnections: 4
-  });
-
-  release(conn)
 });

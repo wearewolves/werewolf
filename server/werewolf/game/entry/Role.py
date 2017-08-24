f87999a5c3eba1fbc0bd3cdfd944aec2dab0fa51
epi <epi4@github.com>
2017-08-24 23:19:34 +0900

    파일 시간 갱신.

--- server/werewolf/game/entry/Role.py
+++ server/werewolf/game/entry/Role.py
@@ -29,7 +29,7 @@ class Truecharacter(object):
     SEER_ODD = 16
     WEREWOLF_CON = 17
 
-    # 더미룰을 위한 더미 리스트 (인랑리스트, 기타리스트) 
+    # 더미룰을 위한 더미 리스트 (인랑리스트, 기타리스트)
     # 더미 인랑 리스트: 랑습룰시 더미 가능
     LIST_WEREWOLF = [WEREWOLF, LONELYWEREWOLF, READERWEREWOLF, WEREWOLF_CON]
     # 더미 기타 리스트: 어떠한 경우에도 더미가 되지 않음

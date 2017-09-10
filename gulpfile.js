7ff7384a6f112bc6a490d02c72d9f1f38ed80db0
epi <epi4@github.com>
2017-09-10 23:59:43 +0900

    3서버 설정용

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,38 @@ var ftp = require('vinyl-ftp');
 var gutil = require('gulp-util');
 var minimist = require('minimist');
 var args = minimist(process.argv.slice(2));
-gulp.task('deploy', function() {
+
+function release(conn) {
   var remotePath = '/www/';
+
+  gulp.src(['release/**/*.*'])
+    .pipe(conn.newer(remotePath))
+    .pipe(conn.dest(remotePath));  
+  gulp.src(['server/next.cgi'])
+    .pipe(conn.mode(remotePath +'server','0755'))
+    .pipe(conn.newer(remotePath + 'server'))
+    .pipe(conn.dest(remotePath + 'server'));
+}
+
+gulp.task('deploy', function() {
   var conn = ftp.create({
-    host: 'werewolf.co.kr',
+    host: 'werewolf6.cafe24.com',
     user: args.user,
     password: args.password,
     log: gutil.log,
     maxConnections: 4
   });
 
-  gulp.src(['release/**/*.*'])
-    .pipe(conn.newer(remotePath))
-    .pipe(conn.dest(remotePath));
+  release(conn)
+});
+gulp.task('staging', function() {
+  var conn = ftp.create({
+    host: 'werewolf7.cafe24.com',
+    user: args.user,
+    password: args.password,
+    log: gutil.log,
+    maxConnections: 4
+  });
 
-  gulp.src(['server/next.cgi'])
-    .pipe(conn.mode(remotePath +'server','0755'))
-    .pipe(conn.newer(remotePath + 'server'))
-    .pipe(conn.dest(remotePath + 'server'));
+  release(conn)
 });

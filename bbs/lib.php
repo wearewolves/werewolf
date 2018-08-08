d13f064bf31e8e350d56f3a41a3fbdf67683bf19
Jakaline <dol2848@naver.com>
2018-08-08 12:22:28 +0900

    lib hotfix

--- bbs/lib.php
+++ bbs/lib.php
@@ -411,7 +411,7 @@ ini_set('session.auto_start','1');
 	<meta http-equiv=Content-Type content=text/html; charset='EUC-KR'>
 	<meta name="keywords" content="인랑,타뷸라,마피아,타뷸라의 늑대,타뷸라의늑대,마피아 게임,마피아게임,추리 게임,추리게임,웹게임">
 	<meta name="description" content="24시간 진행되는 하드코어 마피아 게임 & 타뷸라의 늑대. 웹 추리 게임의 진수를 느껴보세요!">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0/">
 	<meta property="og:type" content="webgame">
 	<meta property="og:title" content="추리 웹게임 인랑">
 	<meta property="og:description" content="24시간 진행되는 하드코어 마피아 게임 & 타뷸라의 늑대. 웹 추리 게임의 진수를 느껴보세요!">

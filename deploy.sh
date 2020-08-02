552b9d01ed0646bd6c804d32bc66a24e0cb3fce4
rekenber <rekenber@gmail.com>
2020-08-03 06:02:13 +0900

    travis ci 사용 종료

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-COMMIT=`git log -1 --no-walk --tags --pretty="%h" --skip=1`
-BEFORE_TAG=`git describe --abbrev=0 --tags $COMMIT`
-`git diff $BEFORE_TAG $3 --name-only |  xargs tar -zcf release.tar.gz`
-`mkdir release`
-`tar -xvzf release.tar.gz -C release`
-
-gulp deploy --user $1 --password $2
--- staging.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git diff HEAD~1 HEAD --name-only |  xargs tar -zcf release.tar.gz
-mkdir release
-tar -xvzf release.tar.gz -C release
-ls release
-gulp staging --user $1 --password $2

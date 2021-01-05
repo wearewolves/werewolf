bbd2b9ce73740e324e3ec841bc3c4fd69575728a
rekenber <rekenber@gmail.com>
2021-01-06 03:13:56 +0900

    구글애널리틱스 설정 변경

--- bbs/lib.php
+++ bbs/lib.php
@@ -407,11 +407,26 @@ ini_set('session.auto_start','1');
 			?>
 <html> 
 <head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-1345394-2"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-1345394-2');
+
+	  gtag('config', 'GA_MEASUREMENT_ID', {
+		  'user_id': '<?=md5($member[no].$member[user_id])?>' 
+		});
+	</script>
+
 	<title>인랑::추리 웹게임</title>
 	<meta http-equiv=Content-Type content=text/html; charset='EUC-KR'>
 	<meta name="keywords" content="인랑,타뷸라,마피아,타뷸라의 늑대,타뷸라의늑대,마피아 게임,마피아게임,추리 게임,추리게임,웹게임">
 	<meta name="description" content="24시간 진행되는 하드코어 마피아 게임 & 타뷸라의 늑대. 웹 추리 게임의 진수를 느껴보세요!">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0/">
+	<meta name="google-site-verification" content="EB7A7bHShLk6McNAcBBrUGAV4XWxhw-xS14Jfc40srk" />
 	<meta property="og:type" content="webgame">
 	<meta property="og:title" content="추리 웹게임 인랑">
 	<meta property="og:description" content="24시간 진행되는 하드코어 마피아 게임 & 타뷸라의 늑대. 웹 추리 게임의 진수를 느껴보세요!">
--- index.php
+++ index.php
@@ -1,22 +1,32 @@
-<? 
-//header( "Location:bbs/zboard.php?id=werewolf" ); 
-// 해킹인가? =ㅅ=ㅋ; 누구삼 바꿔 놓은게?
-?> 
-
 <html>
 	<head>
 		<!-- new 로그분석코드 시작 -->
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-1345394-2"></script>
 		<script>
-		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
 
-		  ga('create', 'UA-1345394-2', 'auto');
-		  ga('send', 'pageview');
+		  gtag('config', 'UA-1345394-2');
 
+		  gtag('config', 'GA_MEASUREMENT_ID', {
+			  'user_id': '<?=md5($member[no].$member[user_id])?>' 
+		  });
 		</script>
+
 		<!-- new 로그분석코드 완료 -->
+		<title>인랑게임</title>
+		<meta http-equiv=Content-Type content=text/html; charset='EUC-KR'>
+		<meta name="keywords" content="인랑,타뷸라,마피아,타뷸라의 늑대,타뷸라의늑대,마피아 게임,마피아게임,추리 게임,추리게임,웹게임">
+		<meta name="description" content="24시간 진행되는 하드코어 마피아 게임 & 타뷸라의 늑대. 웹 추리 게임의 진수를 느껴보세요!">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0/">
+		<meta name="google-site-verification" content="EB7A7bHShLk6McNAcBBrUGAV4XWxhw-xS14Jfc40srk" />
+		<meta property="og:type" content="webgame">
+		<meta property="og:title" content="추리 웹게임 인랑">
+		<meta property="og:description" content="24시간 진행되는 하드코어 마피아 게임 & 타뷸라의 늑대. 웹 추리 게임의 진수를 느껴보세요!">
+		<meta property="og:image" content="http://vignette1.wikia.nocookie.net/werewolf/images/b/bb/Werewolfmain.png/revision/latest/scale-to-width-down/500?cb=20120926180652&path-prefix=ko">
+		<meta property="og:url" content="http://werewolf.co.kr">
 		<script>
 			location.href="bbs/zboard.php?id=werewolf"
 		</script>
